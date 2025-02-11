becf5afa752630f27be6426f3cf9f75084916765
FOX/ <yassinoumouh599@gmail.com>
2025-02-11 15:05:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def check_token():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
